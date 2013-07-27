f4abfe21a4e6631f1c1cbfeabc232c1360da5392
mishababiy <babiymisha@yandex.ru>
2013-07-27 03:48:56 +0300

    Update settings.py

--- test_project/settings.py
+++ test_project/settings.py
@@ -58,15 +58,15 @@ TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
 )
-#TEMPLATE_CONTEXT_PROCESSORS = (
-#    'django.contrib.auth.context_processors.auth',
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
 #    'django.core.context_processors.i18n',
 #    'django.core.context_processors.request',
 #    'django.core.context_processors.media',
 #    'django.core.context_processors.static',
-#)
+)
 TEMPLATE_DIRS = (
-    rel('templates'),
+   # rel('templates'),
 )
 
 MIDDLEWARE_CLASSES = (
